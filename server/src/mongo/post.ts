76b736f6f3f143a2416431b91dd9bb00e4fc693d
tae-jun <i2r.jun@gmail.com>
2014-11-26 23:25:52 +0900

    BoBtudy routes

---
+++ server/src/mongo/post.ts
@@ -0,0 +1,9 @@
+﻿import mongodb = require('mongodb');
+import ObjectID = mongodb.ObjectID;
+
+class post {
+    _id: ObjectID;      // mongodb primary key
+    name: string;       // name
+    date: string        // created date
+    member: ObjectID[]  // users
+}
