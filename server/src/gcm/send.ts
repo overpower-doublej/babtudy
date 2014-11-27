6464b254f22fc343a2061febc4a87cccd869011f
tae-jun <i2r.jun@gmail.com>
2014-11-27 18:32:29 +0900

    -

--- server/src/gcm/send.ts
+++ server/src/gcm/send.ts
@@ -2,7 +2,7 @@
 import configAll = require('../config');
 var config = configAll.gcm;
 
-function send(regId: string, msg: Object, callback: (result) => void) {
+function send(regIds: string[], msg: Object, callback: (result) => void) {
     // Create a message
     var gcmMsg = new gcm.Message({
         collapseKey: Math.round(Math.random() * 10).toString(),     // Must be string
@@ -10,10 +10,10 @@ function send(regId: string, msg: Object, callback: (result) => void) {
         //timeToLive: 3,
         data: msg
     });
+
     // Setup sender
     var sender = new gcm.Sender(config.SERVER_ACCESS_KEY);
-    // Setup target device's registration id as array form
-    var regIds = [regId];
+
     /**
      * Params: message-literal, registrationIds-array, No. of retries, callback-function
      **/
@@ -21,7 +21,6 @@ function send(regId: string, msg: Object, callback: (result) => void) {
         if (err) return console.error(err);
         callback(result);
     });
-
 }
 
 export = send;
